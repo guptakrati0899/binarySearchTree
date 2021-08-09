bae8d760313462d7ad70d0180ad1f5d96d78c162
KratiGupta <kratigupta8545@gmail.com>
2021-08-09 12:20:42 +0530

    Added creation of BST succesfully

---
+++ BinaryNode.java
@@ -0,0 +1,19 @@
+package binarySearchTree;
+
+public class BinaryNode<K extends Comparable<K>> {
+	
+	K key;
+	BinaryNode<K> left;
+	BinaryNode<K> right;
+	
+	
+	public BinaryNode(K key){
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	}
+	
+	
+	
+
+}
---
+++ MyBinaryTreeTest.java
@@ -0,0 +1,22 @@
+package binarySearchTree;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyBinaryTreeTest {
+
+	@Test
+	public void given3NumbersWhenAddedToBinaryTreeShouldReturnSize() {
+		
+		BinaryTree<Integer> BinaryTree = new BinaryTree<>();
+		BinaryTree.add(56);
+		BinaryTree.add(30);
+		BinaryTree.add(70);
+		int size = BinaryTree.getSize();
+		Assert.assertEquals(3, size);
+		
+	}
+
+}
