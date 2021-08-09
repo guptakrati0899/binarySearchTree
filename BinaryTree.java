b24ad31b108b6e38b37bbc620d8c0246138de0d4
KratiGupta <kratigupta8545@gmail.com>
2021-08-09 12:15:18 +0530

    Added Welcome message Successfully

---
+++ BinaryTree.java
@@ -0,0 +1,8 @@
+package binarySearchTree;
+
+public class BinaryTree<K extends Comparable<K>> {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to master branch of BST");
+	}
+}
